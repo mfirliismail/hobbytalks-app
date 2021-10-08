dce30b45f4a4d72df0327fa05e01056f35f95c83
awahids <awahid.safhadi@gmail.com>
2021-10-08 15:01:35 +0800

    + sign up controllers

---
+++ controllers/signUpController.js
@@ -0,0 +1,12 @@
+const { UsersSchema } = require('../models')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+    signUp: async (req, res) => {
+        try {
+            const s
+        } catch (error) {
+            
+        }
+    }
+}
