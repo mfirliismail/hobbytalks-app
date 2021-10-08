65d72d6918a273754d822a8f64d338e8a304bbe0
mfirliismail <m.firliismail@gmail.com>
2021-10-08 15:55:22 +0700

    fix

--- models/Comment.js
+++ models/Comment.js
@@ -3,23 +3,23 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
     userId: {
-        type: Schema.Type.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Users"
     },
     threadId: {
-        type: Schema.Type.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Threads"
     },
     content: {
         type: String
     },
     likes: [{
-        type: Schema.Type.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Users"
     }],
     dislike: [{
 
-        type: Schema.Type.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Users"
 
     }]
