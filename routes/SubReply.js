97a94097ae4023872c64bca4884e2535a13a2298
FIBastian <ivanbastian13@gmail.com>
2021-10-11 17:46:37 +0700

    +update route subReply

--- routes/SubReply.js
+++ routes/SubReply.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const SubReply = require('../controllers/SubReplyController')
+const { authToken } = require('../middlewares/auth')
 
-router.post("/:replyId", SubReply.createSubReply)
+router.post("/:replyId", authToken, SubReply.createSubReply)
 router.get("/", SubReply.readAllSubReply)
-router.get("/:id", SubReply.readOneSubReply)
-router.put("/:id", SubReply.updateSubReply)
-router.delete("/:id", SubReply.deleteSubReply)
+router.put("/:id", authToken, SubReply.updateSubReply)
+router.delete("/:id", authToken, SubReply.deleteSubReply)
 
 module.exports = router
