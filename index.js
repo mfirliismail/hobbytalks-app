aa38c1d8c8b1aa8db743bb7a3046c5781b40743f
mfirliismail <m.firliismail@gmail.com>
2021-10-17 00:57:00 +0700

    cors fix

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const port = process.env.PORT || 5000
 const router = require('./routes')
 const db = require('./db/database')
-app.use(express.json())
+
 const passport = require('./middlewares/passport')
 const session = require('cookie-session')
 const cors = require('cors')
-
+app.use(express.json())
+app.use(cors())
 db()
 app.use(session({
     name: "userlogin-cookie",
@@ -16,16 +17,6 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use('/api/v1', router)
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 app.get('/', (req, res) => {
     return res.status(200).json({
