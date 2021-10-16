f2ace06eaa2e36d7049c374928b802fd577440ce
mfirliismail <m.firliismail@gmail.com>
2021-10-17 00:14:05 +0700

    deploy test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./db/database')
 app.use(express.json())
 const passport = require('./middlewares/passport')
 const session = require('cookie-session')
-
+const cors = require('cors')
 
 db()
 app.use(session({
@@ -16,8 +16,16 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use('/api/v1', router)
+app.use(cors())
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.get('/', (req, res) => {
     return res.status(200).json({
