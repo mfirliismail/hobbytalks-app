a52aa3ac099d56af2b7ceb17b309806c9c62a181
mfirliismail <m.firliismail@gmail.com>
2021-10-29 08:27:17 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 const router = require('./routes')
 const db = require('./db/database')
 const passport = require('./middlewares/passport')
